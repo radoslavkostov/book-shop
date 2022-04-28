369c8260c0ae51be8b19d9bceab99c2fc8289d50
radoslavkostov <103312630+radoslavkostov@users.noreply.github.com>
2022-04-28 23:36:26 +0300

    Authorization
    Most of the structure of the project, Authorization with Firebase

--- book-shop/src/environments/environment.prod.ts
+++ book-shop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8F7jSdFBxA19pI0oq1zlo70AbHfG-s1w",
+    authDomain: "book-shop-97e38.firebaseapp.com",
+    projectId: "book-shop-97e38",
+    storageBucket: "book-shop-97e38.appspot.com",
+    messagingSenderId: "693721850708",
+    appId: "1:693721850708:web:f01a0e662ace93a9d29ec1"
+  }
 };
--- book-shop/src/environments/environment.ts
+++ book-shop/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8F7jSdFBxA19pI0oq1zlo70AbHfG-s1w",
+    authDomain: "book-shop-97e38.firebaseapp.com",
+    projectId: "book-shop-97e38",
+    storageBucket: "book-shop-97e38.appspot.com",
+    messagingSenderId: "693721850708",
+    appId: "1:693721850708:web:f01a0e662ace93a9d29ec1"
+  }
 };
 
 /*
